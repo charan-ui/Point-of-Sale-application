f2e931699f7cbc8c229616cff7f0e3be12310d01
hurulichikkanahall_c <hurulichikkanahall.c@husky.neu.edu>
2020-04-18 20:32:26 -0400

    db changes

--- connectdb.php
+++ connectdb.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $pdo=new PDO('mysql:host=localhost;dbname=pos_db','root','');
+    $pdo=new PDO('mysql:host=i2cpbxbi4neiupid.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=bb55r5hjop4grcdo','x84md51dk02kq0bo','leumgji4g3vn4xo8');
 
     
 }catch(PDOException $f){
