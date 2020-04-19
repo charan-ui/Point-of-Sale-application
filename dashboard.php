d45d5101847e09141280f4f5ddafbea976a7f544
hurulichikkanahall_c <hurulichikkanahall.c@husky.neu.edu>
2020-04-19 10:17:47 -0400

    page load

--- dashboard.php
+++ dashboard.php
@@ -75,7 +75,7 @@ include_once'header.php';
       <!--------------------------
         | Your Page Content Here |
         -------------------------->
-        <marquee behavior="alternate" >Please Stay Home!</marquee>
+        
      <div class="box-body">
          
         <div class="row">
